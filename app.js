4ff183132f3d6b71c9fd2d6b6a2a37f7ce35c81a
Nelson Pacheco <99364660+neIsonp@users.noreply.github.com>
2023-04-23 20:02:45 +0000

    fix the time bug

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-const daysText = document.querySelector('#days')
-const hoursText = document.querySelector('#hours')
-const minutesText = document.querySelector('#minutes')
-const secondsText = document.querySelector('#seconds')
-
-const newYearsDate = new Date('1 Jan 2023')
-
-setInterval(countdown, 1000)
+const launchDate = new Date("2023-06-01T12:00:00Z");
 
 function countdown() {
-  const { days, hours, minutes, seconds } = precessCountdownValues()
-  renderCountdown({ days, hours, minutes, seconds })
+
+  const currentDate = new Date();
+
+
+  const diff = launchDate - currentDate;
+
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  let minutes = Math.floor((diff / 1000 / 60) % 60);
+  let seconds = Math.floor((diff / 1000) % 60);
+
+
+  document.getElementById("days").innerHTML = days < 10 ? "0" + days : days;
+  document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
 }
 
-function addPadStart(value) {
-  return String(value).padStart(2, '0');
-}
 
+setInterval(countdown, 1000);
 
-function precessCountdownValues() {
-  const currentDate = new Date()
-  const totalSeconds = (newYearsDate - currentDate) / 1000
-
-  const days = Math.floor((totalSeconds / 3600) / 24)
-  const hours = Math.floor((totalSeconds / 3600) % 24)
-  const minutes = Math.floor((totalSeconds / 60) % 60)
-  const seconds = Math.floor(totalSeconds % 60)
-
-  return { days, hours, minutes, seconds }
-
-}
-
-function renderCountdown({ days, hours, minutes, seconds }) {
-  daysText.innerText = addPadStart(days).substring(1)
-  hoursText.innerText = addPadStart(hours).substring(1)
-  minutesText.innerText = addPadStart(minutes).substring(1)
-  secondsText.innerText = addPadStart(seconds).substring(1)
-}
